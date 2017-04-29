db40b46814232c1c69ce0fc50ca5d138d8943b8d
Daniel Mateus Pires <dmateusp@gmail.com>
2017-04-29 16:26:09 +0100

    Gravity data pushed

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url : "mongodb://localhost:27017/db"
+  url : "mongodb://localhost:27017/icy-poles"
 };
--- src/routes/note_routes.js
+++ src/routes/note_routes.js
@@ -1,13 +1,25 @@
 module.exports = function(app, db) {
   app.get('/ice/', (req, res) => {
     console.log(db)
-    db.collection['ice_data'].find((err, items) => {
+    db.collection['ice'].find((err, items) => {
+      if (err) {
+        res.send({'error':'An error has occurred'})
+      } else {
+        res.send(items)
+      }
+    });
+
+  });
+  app.get('/gravity/', (req, res) => {
+    console.log(db)
+    db.collection('gravity').find({}, {}, { limit:100 }).toArray(function(err, items) {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
-        res.send(items);
+        console.log(items)
+        res.send(items)
       }
-    });
+    })
 
   });
 };
