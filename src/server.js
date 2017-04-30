dcaf1ed697867df79076fba8799e034a6126b373
Daniel Mateus Pires <dmateusp@gmail.com>
2017-04-30 10:03:59 +0100

    Adding $PORT environment variable for heroku

--- src/server.js
+++ src/server.js
@@ -7,14 +7,14 @@ const db             = require('./config/db')
 
 const app            = express()
 
-const port = 80
+if (typeof process.env.PORT === 'undefined') process.env.PORT = '8000'
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) {console.log(err);  return console.log("Error DB not connecting")}
 
   require('./routes')(app, database)
 
-  app.listen(port, () => {
-    console.log('We are live on ' + port)
+  app.listen(process.env.PORT, () => {
+    console.log('We are live on ' + process.env.PORT)
   });
 })
