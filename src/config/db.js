8bd552925593b40b9f4e661dc2b67d2d4bdf78cd
Daniel Mateus Pires <dmateusp@gmail.com>
2017-04-30 09:02:55 +0100

    Changes on exploration work

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url : "mongodb://0.tcp.ngrok.io:13855/icy-poles"
+  url : "mongodb://0.tcp.ngrok.io:13033/icy-poles"
+  // url : "mongodb://localhost:27017/icy-poles" // For local DB
 };
